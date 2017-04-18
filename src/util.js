6c02ff29f5bd58067c0f616dcfbded4e724742b0
Sergey <skurdin@yahoo.com>
2017-04-18 11:07:40 -0400

    Removed vnode.attributes code not related to inferno

--- src/util.js
+++ src/util.js
@@ -4,6 +4,6 @@ export function assign(obj, props) {
 }
 
 export function getKey(vnode, fallback) {
-	var key = vnode.key || (vnode.attributes && vnode.attributes.key);
+	let key = vnode.key;
 	return key===null || key===undefined ? fallback : key;
 }
